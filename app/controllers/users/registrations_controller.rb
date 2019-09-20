8713932ec88395947c2f53e43d6bdae1ed179bea
Ayush <ayagrawal@grepruby.io>
2019-09-20 19:46:26 +0530

    final signup login forget-password page with proper validations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,1 +1,30 @@
-class Users::RegistrationsController < Devise::RegistrationsController; end
+class Users::RegistrationsController < Devise::RegistrationsController; 
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      flash[:error] = resource.errors.full_messages.join("<br/>").html_safe
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+  # def create 
+  #   super
+  #   flash[:error] = resource.errors.full_messages.join("<br/>").html_safe
+  # end
+end
+
